8c109fc0ec5112382664580c8f09d282b3e761ab
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-12-02 18:57:41 +0530

    time stamp

--- src/js/index.js
+++ src/js/index.js
@@ -1,60 +1,72 @@
-/* This is a function that is listening for the DOM to be ready. */
-document.addEventListener("DOMContentLoaded", () => {
-  initApp();
-});
+timer();
+sidebarMenu();
+owlCarousel();
 
-const initApp = () => {};
+function owlCarousel() {
+  $(".banner-owl-carousel").owlCarousel({
+    loop: true,
+    items: 1,
+    dots: false,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+  });
 
-// querySelectorAll function
-function qsAll(selector, parent = document) {
-  return [...parent.querySelectorAll(selector)];
+  $(".neostorefurniture-owl-carousel").owlCarousel({
+    loop: true,
+    items: 1,
+    dots: false,
+    margin: 10,
+    nav: false,
+
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 3,
+      },
+      1000: {
+        items: 6,
+      },
+    },
+  });
 }
 
-// querySelector function
-function qs(selector, parent = document) {
-  return parent.querySelector(selector);
+function sidebarMenu() {
+  const sideMenu = document.querySelector("#sideMenu"),
+    sidebar = document.querySelector("#sidebar"),
+    closeMenu = document.querySelector("#closeMenu");
+
+  sideMenu.addEventListener("click", () => {
+    sidebar.style.display = "block";
+  });
+
+  closeMenu.addEventListener("click", () => {
+    sidebar.style.display = "none";
+    // location.reload();
+  });
 }
 
-$(".banner-owl-carousel").owlCarousel({
-  loop: true,
-  items: 1,
-  dots: false,
-  margin: 10,
-  nav: true,
-  autoplay: true,
-  autoplayTimeout: 2000,
-  autoplayHoverPause: true,
-});
+function timer() {
+  const countDownDate = new Date("Dec 5, 2022 15:37:25").getTime();
+  const x = setInterval(function () {
+    const now = new Date().getTime(),
+      distance = countDownDate - now,
+      days = Math.floor(distance / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-$(".neostorefurniture-owl-carousel").owlCarousel({
-  loop: true,
-  items: 1,
-  dots: false,
-  margin: 10,
-  nav: false,
+    document.getElementById(
+      "timer"
+    ).innerHTML = `end in ${days}d : ${hours}h : ${minutes}m : ${seconds}s`;
 
-  responsive: {
-    0: {
-      items: 1,
-    },
-    576: {
-      items: 3,
-    },
-    1000: {
-      items: 6,
-    },
-  },
-});
-
-const sideMenu = document.querySelector("#sideMenu");
-const sidebar = document.querySelector("#sidebar");
-const closeMenu = document.querySelector("#closeMenu");
-
-sideMenu.addEventListener("click", () => {
-  sidebar.style.display = "block";
-});
-
-closeMenu.addEventListener("click", () => {
-  sidebar.style.display = "none";
-  // location.reload();
-});
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+}
