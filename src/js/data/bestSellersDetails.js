388ab27e2dbf90aa7418463d87d13fcbe9f74fee
Rohit Kumar <rohit97751@gmail.com>
2022-12-03 01:51:54 +0530

    BESTSELLERS array of object

---
+++ src/js/data/bestSellersDetails.js
@@ -0,0 +1,44 @@
+export const bestSellersDetails = [
+  {
+    id: 1,
+    productImage: "src/images/white-wooden-chair-1.png",
+    title: " White Wooden Chair",
+    price: "₹8000.00",
+    discount: "₹6000.00",
+  },
+  {
+    id: 2,
+    productImage: "src/images/white-wooden-chair-2.png",
+    title: " White Wooden Chair",
+    price: "₹8000.00",
+    discount: "₹6000.00",
+  },
+  {
+    id: 3,
+    productImage: "src/images/white-wooden-chair-3.png",
+    title: " White Wooden Chair",
+    price: "₹8000.00",
+    discount: "₹6000.00",
+  },
+  {
+    id: 4,
+    productImage: "src/images/white-wooden-chair-4.png",
+    title: " White Wooden Chair",
+    price: "₹8000.00",
+    discount: "₹6000.00",
+  },
+  {
+    id: 5,
+    productImage: "src/images/white-wooden-chair-5.png",
+    title: " White Wooden Chair",
+    price: "₹8000.00",
+    discount: "₹6000.00",
+  },
+  {
+    id: 6,
+    productImage: "src/images/white-wooden-chair-6.png",
+    title: " White Wooden Chair",
+    price: "₹8000.00",
+    discount: "₹6000.00",
+  },
+];
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
+import { bestSellersDetails } from "./data/bestSellersDetails.js";
+
 timer();
 sidebarMenu();
 owlCarousel();
+bestSellersProductList(bestSellersDetails);
+likeHeartButton();
 
 function owlCarousel() {
   $(".banner-owl-carousel").owlCarousel({
@@ -69,4 +73,62 @@ function timer() {
       document.getElementById("timer").innerHTML = "EXPIRED";
     }
   }, 1000);
+}
+
+function likeHeartButton() {
+  const hearts = document.querySelectorAll(".heart");
+
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", function (e) {
+      heart.classList.toggle("toggleRedColor");
+    });
+  });
+}
+
+function bestSellersProductList(bestSellersDetail) {
+  const bestSellersProduct = document.getElementById("bestSellersProduct");
+
+  bestSellersDetail.forEach((product) => {
+    let html = `<div class="col-12 col-sm-6 col-lg-4 my-2">
+    <div class="card border-0">
+      <div class="d-lg-flex align-items-center justify-content-center">
+        <img src=${product.productImage} class="card-img-top rounded-0" alt="White Wooden Chair"
+          title=" White Wooden Chair" />
+
+        <div
+          class="card-body w-100 h-auto d-flex flex-column align-items-start justify-content-between pb-4 gap-2">
+          <div class="d-flex align-items-center justify-content-start gap-1">
+            <span class="icon-full-start"></span>
+            <span class="icon-full-start"></span>
+            <span class="icon-full-start"></span>
+            <span class="icon-full-start"></span>
+            <span class="icon-full-start"></span>
+          </div>
+
+          <div>
+            <h5 class="card-title mb-1 text-black-800 fw-500 fs-14">
+              ${product.title}
+            </h5>
+            <div class="card-text fw-500 fs-14">
+              <s class="text-black-400">${product.price}</s>
+              <span class="text-black-800">${product.discount}</span>
+            </div>
+          </div>
+
+          <div>
+            <button class="heart bg-transparent">
+              <span class="icon-Shape-1"></span>
+            </button>
+
+            <button class="vector-2 bg-transparent">
+              <span class="icon-Vector-2"></span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+    bestSellersProduct.innerHTML += html;
+  });
 }
